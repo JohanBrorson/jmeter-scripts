0eb6f57e88431c37dbac19e32bfe46d984c597ef
Johan Brorson <johbro@spotify.com>
2014-10-08 09:44:39 +0200

    Add scripts

---
+++ jmeter-reporter.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+PERCENTILE_NUMBER=95
+
+function usage {
+  echo "Usage: `basename $0` -j test_result"
+  echo -e "\t-j\tTest result (.jtl file)"
+  exit 1
+}
+
+function get_elapsed_time {
+  local START_TIME=`head -n 1 $JTL_FILE | cut -d "," -f1`
+  local END_TIME=`tail -n 1 $JTL_FILE | cut -d "," -f1`
+  printf "%.0f" $(echo "scale=3;($END_TIME - $START_TIME)/1000" | bc)
+}
+
+function print_table_row {
+  printf "  %-30s %20s\n" "$1" "$2"
+}
+
+while getopts ":j:" OPTION
+do
+  case $OPTION in
+    j ) JTL_FILE=$OPTARG;;
+    * ) usage;; # Default option
+  esac
+done
+
+if [[ -z $JTL_FILE ]]; then
+  usage
+fi
+
+if [ ! -f $JTL_FILE ]; then
+  echo "ERROR The file $JTL_FILE doesn't exist!"
+fi
+
+echo "PERFORMANCE TEST REPORT"
+
+ELAPSED_TIME=`get_elapsed_time`
+HOURS=`echo "$ELAPSED_TIME / 3600" | bc`
+MINUTES=`echo "($ELAPSED_TIME - ($HOURS * 3600)) / 60" | bc`
+SECONDS=`echo "$ELAPSED_TIME - ($HOURS * 3600) - ($MINUTES * 60)" | bc`
+print_table_row "Elapsed time:" "$HOURS h, $MINUTES m, $SECONDS s"
+
+NUMBER_OF_REQUESTS=`grep -c -e '^[0-9]\{13\},.*$' $JTL_FILE`
+print_table_row "Total number of calls" $NUMBER_OF_REQUESTS
+
+CALLS_PER_SECOND=`printf %.2f $(echo "scale=4;$NUMBER_OF_REQUESTS / $ELAPSED_TIME" | bc)`
+print_table_row "Calls per second" $CALLS_PER_SECOND
+
+NUMBER_OF_ERRORS=`awk -F',' 'int($4)>=400' $JTL_FILE | wc -l`
+print_table_row "Number of errors" $NUMBER_OF_ERRORS
+
+ERROR_PERCENT=`printf %.1f $(echo "scale=3;$NUMBER_OF_ERRORS * 100/ $NUMBER_OF_REQUESTS" | bc)`
+print_table_row "Percentage share of errors" "$ERROR_PERCENT %"
+
+IFS=$'\n'
+for REQUEST in `cut -d',' -f3 $JTL_FILE | sort | uniq`;
+do
+  unset RESPONSE_TIMES
+  unset RESPONSE_TIMES_SORTED
+  unset PERCENTILE_ELEMENT
+
+  echo -e "\nRequest: $REQUEST"
+
+  COUNT=`grep -c ",$REQUEST," $JTL_FILE`
+  print_table_row "Number of calls" $COUNT
+
+  # Create an array with the response times and sum the response times
+  i=0
+  TOTAL=0
+  for RESPONSE_TIME in `grep ",$REQUEST," $JTL_FILE | cut -d',' -f2`; do
+    RESPONSE_TIMES[i]=$RESPONSE_TIME
+    let "i += 1"
+    let "TOTAL += $RESPONSE_TIME"
+  done
+
+  # Calculate average with three decimals and round it to an integer
+  AVERAGE=$(printf %.0f `echo "scale=3;$TOTAL / $COUNT" | bc`)
+  print_table_row "Average response time" $AVERAGE
+
+  # Get the percentile
+  RESPONSE_TIMES_SORTED=(`echo ${RESPONSE_TIMES[@]} | sed 's/ /\n/g' | sort -n`)
+  PERCENTILE_ELEMENT=`echo "scale=2;$PERCENTILE_NUMBER / 100 * ${#RESPONSE_TIMES_SORTED[@]}" | bc`
+  PERCENTILE_ELEMENT_INTEGER_PART=${PERCENTILE_ELEMENT/.*}
+  case `echo "$PERCENTILE_ELEMENT_INTEGER_PART == $PERCENTILE_ELEMENT" | bc` in
+    1) ELEMENT=$(($PERCENTILE_ELEMENT_INTEGER_PART - 1));;
+    0) ELEMENT=$PERCENTILE_ELEMENT_INTEGER_PART;;
+  esac
+  print_table_row "${PERCENTILE_NUMBER}th percentile" ${RESPONSE_TIMES_SORTED[$ELEMENT]}
+
+  MIN=${RESPONSE_TIMES_SORTED[0]}
+  print_table_row "Min response time" $MIN
+
+  MAX=${RESPONSE_TIMES_SORTED[@]:(-1)}
+  print_table_row "Max response time" $MAX
+
+done
+unset IFS
---
+++ jmeter-runner.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+LOGLEVEL=INFO
+RESULTS_DIR=results
+
+function usage {
+  echo "Usage: `basename $0` -t test_plan [-q additional_properties_file]"
+  echo -e "\t-t\tTest plan (.jmx file)"
+  echo -e "\t-q\tAdditional properties file (optional)"
+  exit 1
+}
+
+function log_error {
+  echo "ERROR: $*"
+  exit 1
+}
+
+# Parse command line arguments
+while getopts ":t:q:" OPTION
+do
+  case $OPTION in
+    t ) JMX_FILE=$OPTARG;;
+    q ) PROPERTIES_FILE=$OPTARG;;
+    * ) usage;; # Default option
+  esac
+done
+
+# Check that the variable has been set
+if [[ -z $JMX_FILE ]]; then
+  usage
+fi
+
+# Check if the JMX file exist
+if [ ! -f $JMX_FILE ]; then
+  log_error "The file $JMX_FILE doesn't exist!"
+fi
+
+LOGFILE="$RESULTS_DIR/`date '+%Y%m%d%H%M'`-`basename ${JMX_FILE%\.*}`.log"
+JTLFILE="$RESULTS_DIR/`date '+%Y%m%d%H%M'`-`basename ${JMX_FILE%\.*}`.jtl"
+
+if [ ! -d "$RESULTS_DIR" ]; then
+  mkdir "$RESULTS_DIR"
+fi
+
+# Run JMeter
+java -jar `dirname $0`/ApacheJMeter.jar -n -t $JMX_FILE -L $LOGLEVEL -j $LOGFILE -l $JTLFILE -q $PROPERTIES_FILE
